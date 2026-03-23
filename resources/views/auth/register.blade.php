b4153820357e112c8cafdea1b1a9dd03cbee5f23
octomagician <anadeleonazp@gmail.com>
2026-03-23 14:14:04 -0600

    Changed: Register view changed for make already registered? visible

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,8 @@
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
+        </div>
+        <div lass="flex items-center justify-end mt-4">
             <div class="mt-4">
                 <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
                 <x-input-error :messages="$errors->get('g-recaptcha-response')" class="mt-2" />
